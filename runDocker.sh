f93ead114344310c55fcf369fa79cc4ce636ab8c
Felipe Guerra <administrador@vidasaudavelorganicos.com.br>
2016-01-08 15:24:24 -0200

    Update scripts

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 3000:3000 698115382733.dkr.ecr.us-east-1.amazonaws.com/vsorganicos/coreapi
+docker run -d -p 3000:3000 698115382733.dkr.ecr.us-east-1.amazonaws.com/core
